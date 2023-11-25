b22ac5776f3fb33df7e601a5d9e8c8585c955c85
Soniya Dayanithi <Soniya Dayanithi@SoniyaDayanithi>
2023-11-25 11:10:56 +0530

    First commit...

---
+++ src/newpackage/newhubGitclass.java
@@ -0,0 +1,19 @@
+package newpackage;
+
+public class newhubGitclass {
+
+	public static void main(String[] args) {
+		int a[]=new int [4];
+		a[0]=5;
+		a[1]=10;
+	    a[2]=15;
+	    a[3]=20;
+	    System.out.println(a[1]);
+	    int length = a.length;
+	    System.out.println(length);}
+	}
+				
+				
+
+
+
